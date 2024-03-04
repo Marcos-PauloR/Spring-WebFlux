c08ad7ac6c7d4bdc72c3c59f29912b4ce3b8fd0f
Marcos Paulo R <marcos_pa32@hotmail.com>
2024-03-04 13:12:43 -0300

    First Commit

---
+++ src/test/java/br/com/estudos/webflux/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudos.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
